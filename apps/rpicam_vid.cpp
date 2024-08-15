59390b0b727f539d663b53c6c599d9ae58d8702c
evya oren <evyatarobd@gmail.com>
2024-08-15 10:52:14 +0300

    9

--- apps/rpicam_vid.cpp
+++ apps/rpicam_vid.cpp
@@ -118,7 +118,7 @@ static void event_loop(RPiCamEncoder &app)
 			// output->Signal();
 		// if (!key)
 		// key = param[0];
-		printf(param);
+		std::printf(param);
 		if (param == "1" || param == "2" || param == "3" || param == "4" || param == "5" || param == "6" || param == "7" || param == "8" || param == "9" || param == "0")
 		{
 			num += param;
